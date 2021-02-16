e38a0600a6a8052251cad312032116f84522aede
Scott Shealy <vsshealy@gmail.com>
2021-02-16 18:34:07 -0500

    Added footer styles

--- footer.php
+++ footer.php
@@ -7,3 +7,61 @@
      * @copyright 02/21
     **/
 ?>
+
+<footer id="footer-primary">
+
+    <!-- #FOOTER-MAIN -->
+    <div id="footer-main" class="row">
+        <div class="columns col-xl-12 wrapper-content wrapper-footer">
+            <div class="row">
+                <div class="columns col-sm-12 col-md-6 col-lg-5">
+                    <div class="row">
+                        <div class="columns col-xl-12">
+                            <a href="/" class="logo">
+                                <img src="<?php echo get_template_directory_uri(); ?>/src/svg/logos/logo-seem-full-white.svg" alt="Southeast Energy Exchange Market"/>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="columns col-xl-12">
+                            <p>A group of energy companies serving electricity customers across a wide geographic region in the southeastern U.S. is exploring an integrated, automated intra-hour energy exchange with goals of lowering costs to customers, optimizing renewable energy resources and helping maintain the reliable service we provide today.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="columns col-sm-12 col-md-6 col-lg-7">
+                    <div class="row">
+                        <div class="columns col-sm-12 col-md-6">
+                            <h6>Quick Links</h6>
+                            <ul>
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/news">Newsroom</a></li>
+                                <li><a href="/stakeholders">Stakeholders</a></li>
+                                <li><a href="/faq">FAQ</a></li>
+                                <li><a href="/#contact">Contact Us</a></li>
+                                <li><a href="/privacy">Privacy</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="columns col-sm-12 col-md-6">
+                            <h6>Contact</h6>
+                            <ul>
+                                <li><a href="mailto:seeminfo@southernco.com">SEEMinfo@southernco.com</a></li>
+                                <li><a href="mailto:info@southeastenergymarket.com">info@southeastenergymarket.com</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- #FOOTER-COPYRIGHT -->
+    <div id="footer-copyright" class="row">
+        <div class="columns col-xl-12 wrapper-content">
+            <p>&copy; 2021 Southeast Energy Exchange Market. All Rights Reserved.</p>
+        </div>
+    </div>
+
+</footer>
